274da2a4abe9e5d11337103b030e69fa6cb80d1a
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2024-07-19 12:55:55 +0300

    Layout

--- Task2_2/Calculator.java
+++ Task2_2/Calculator.java
@@ -1,5 +1,7 @@
 package Task2_2;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -39,13 +41,56 @@ public class Calculator extends JFrame {
         this.setSize(500, 400);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        this.setLayout(new GridBagLayout());
+        GridBagLayout gbl = new GridBagLayout();
         GridBagConstraints gbc = new GridBagConstraints();
+        gbc.weightx = (double) 1 / 4;
+        gbc.weighty = (double) 1 / 6;
+        gbc.fill = GridBagConstraints.BOTH;
+        
 
+        this.setLayout(gbl);
+
+        // Label
+        lblMain.setOpaque(true);
+        lblMain.setBackground(Color.LIGHT_GRAY);
+        lblMain.setAlignmentX(RIGHT_ALIGNMENT);
+        lblMain.setAlignmentY(CENTER_ALIGNMENT);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 2;
+        this.add(lblMain, gbc);
+
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+
+        // OPs
         gbc.gridx = 3;
         gbc.gridy = 5;
         this.add(btnEq, gbc);
 
+        gbc.gridx = 3;
+        gbc.gridy = 4;
+        this.add(btnPlus, gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 3;
+        this.add(btnMinus, gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 2;
+        this.add(btnTimes, gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        this.add(btnDivide, gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        this.add(btnPercent, gbc);
+
+
+        // 0, 1-3
         gbc.gridx = 1;
         gbc.gridy = 5;
         this.add(btn0, gbc);
@@ -61,6 +106,33 @@ public class Calculator extends JFrame {
         gbc.gridx = 2;
         gbc.gridy = 4;
         this.add(btn3, gbc);
+
+        // 4-6
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        this.add(btn4, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        this.add(btn5, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        this.add(btn6, gbc);
+
+        // 7-9
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        this.add(btn7, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        this.add(btn8, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        this.add(btn9, gbc);
+
 
 
         this.setVisible(true);
